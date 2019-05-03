e5451365cf150008e31764a10ace0f0d3821ced1
Артем Марков <a.markov@realweb.ru>
2019-05-04 01:33:47 +0300

    add telegram bot

--- rw_got/apps/telegram/views.py
+++ rw_got/apps/telegram/views.py
@@ -1,9 +1,14 @@
+from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 
 from rw_got.apps.telegram.bot import Bot
 
 
 class WebhookView(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(WebhookView, self).dispatch(request, *args, **kwargs)
 
     def post(self, request):
         Bot().accept_message(request.body)
