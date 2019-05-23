a8ef1753f497f1bc6b9ced6ed13602d1db7e418f
Артем Марков <a.markov@realweb.ru>
2019-05-23 19:34:26 +0300

    add handlers

--- rw_got/apps/telegram/incoming_messages_handlers.py
+++ rw_got/apps/telegram/incoming_messages_handlers.py
@@ -1,7 +1,22 @@
 from rw_got.apps.telegram.models import OutgoingMessage
 
 
-def handle_message(message):
+def any_message_trigger(_):
+    return True
+
+
+def hodor_reaction(message):
     OutgoingMessage.objects.create(chat=message.chat,
                                    reply_to=message,
                                    text="Ходор!")
+
+
+handlers = ((any_message_trigger, hodor_reaction),
+            )
+
+
+def handle_message(message):
+    for trigger, reaction in handlers:
+        if trigger(message):
+            reaction(message)
+            break
